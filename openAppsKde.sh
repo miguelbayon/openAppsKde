7624360c1f0da82f7c679f0f268ab013e239444a
Miguel Bayón <publi.michelbayon@gmail.com>
2017-07-20 11:31:54 +0200

    Commit inicial

---
+++ openAppsKde.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+kstart --desktop 1 --windowclass Google-chrome google-chrome-stable --profile-directory="Profile 2"
+sleep 2s
+kstart --desktop 4 --windowclass TelegramDesktop telegram-desktop
+sleep 2s
+kstart --desktop 5 --windowclass dolphin dolphin
+sleep 2s
+kstart --desktop 5 --windowclass dolphin dolphin
+sleep 2s
+kstart --desktop 6 --windowclass Konsole konsole
+sleep 2s
+kstart --desktop 2 --windowclass Google-chrome google-chrome-stable --profile-directory="Default"
+sleep 2s
+kstart --desktop 3 --windowclass Google-chrome google-chrome-stable --profile-directory="Profile 1"
